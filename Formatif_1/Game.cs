48b9d7724044be9727ed214d4f9662bd72a1efd6
Samuel Bolduc <sambolduc4@gmail.com>
2020-02-17 16:37:41 -0500

    Observable Collection, ListView and more....

--- Formatif_1/Game.cs
+++ Formatif_1/Game.cs
@@ -4,14 +4,91 @@ using Formatif_1.Annotations;
 
 namespace Formatif_1
 {
-    public class Game
+    public class Game : INotifyPropertyChanged
     {
-        public string Title { get; set; }
-        public string Editor { get; set; }
-        public string Description { get; set; }
-        public int ReleaseDate { get; set; }
-        public string Console { get; set; }
-        public string CoverPath { get; set; }
-        public double Cote { get; set; }
+        private string title_, editor_, description_, console_, coverpath_;
+        private double rate_;
+        private int releasedate_;
+
+        public string Title
+        {
+            get => title_;
+            set
+            {
+                title_ = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Info));
+            }
+        }
+
+        public string Editor
+        {
+            get => editor_;
+            set
+            {
+                editor_ = value;
+                OnPropertyChanged();
+            }
+        }        
+        public string Description
+        {
+            get => description_;
+            set
+            {
+                description_ = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Info));
+            }
+        }          
+        
+        public int ReleaseDate
+        {
+            get => releasedate_;
+            set
+            {
+                releasedate_ = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public string Console
+        {
+            get => console_;
+            set
+            {
+                console_ = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public string CoverPath
+        {
+            get => coverpath_;
+            set
+            {
+                coverpath_ = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Rate
+        {
+            get => rate_;
+            set
+            {
+                rate_ = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public string Info => $"{Title} : {Description}";
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- Formatif_1/MainWindow.xaml.cs
+++ Formatif_1/MainWindow.xaml.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using Formatif_1.Annotations;
+using Microsoft.Win32;
 
 namespace Formatif_1
 {
     
     public partial class MainWindow : INotifyPropertyChanged
     {
-        public List<Game> Games = new List<Game>();
+        
+        public ObservableCollection<Game> Games { get; set; } = new ObservableCollection<Game>();
 
         private Game currentGame;
 
@@ -33,7 +40,7 @@ namespace Formatif_1
                 Description = "Je test une description :)",
                 ReleaseDate = 2014,
                 Console = "PS4",
-                Cote = 5,
+                Rate = 5,
                 CoverPath = "images/shadow_of_mordor.jpg"
             });
             
@@ -44,7 +51,7 @@ namespace Formatif_1
                 Description = "Cool la description",
                 ReleaseDate = 2016,
                 Console = "XBox",
-                Cote = 8,
+                Rate = 8,
                 CoverPath = "images/fortnite.jpg"
             });
             
@@ -55,7 +62,7 @@ namespace Formatif_1
                 Description = "C vrm nice comme jeu",
                 ReleaseDate = 207,
                 Console = "XBox",
-                Cote = 7,
+                Rate = 7,
                 CoverPath = "images/bf4.jpg"
             });
             
@@ -66,7 +73,7 @@ namespace Formatif_1
                 Description = "Un jeu de cube",
                 ReleaseDate = 2009,
                 Console = "Switch",
-                Cote = 6,
+                Rate = 6,
                 CoverPath = "images/minecraft.jpg"
             });
 
@@ -78,7 +85,9 @@ namespace Formatif_1
 
         private void onChange(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            this.CurrentGame = Games[Convert.ToInt32(e.NewValue)];
+            var newIndex = Convert.ToInt32(e.NewValue);
+            this.CurrentGame = Games[newIndex];
+            this.listView.SelectedIndex = newIndex;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -87,6 +96,89 @@ namespace Formatif_1
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void onGameChange(object sender, SelectionChangedEventArgs e)
+        {
+            this.CurrentGame = e.AddedItems[0] as Game;
+            if(this.slider != null)
+                this.slider.Value = Games.IndexOf(CurrentGame);
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            slider.Maximum += 1;
+            Games.Add(new Game()
+            {
+                Title = "Undefined",
+                Console = "Undefined",
+                Rate = 0,
+                CoverPath = "Undefined",
+                Description = "Undefined",
+                Editor = "Undefined",
+                ReleaseDate = 1970
+            });
+        }
+
+        private void TextboxEdit(object sender, MouseButtonEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            textBox.IsReadOnly = false;
+            textBox.BorderThickness = new Thickness(1);
+        }
+        
+        private void TextboxLostFocus(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var textBox = (TextBox)sender;
+            textBox.IsReadOnly = true;
+            textBox.BorderThickness = new Thickness(0);
+        }
+        
+        private void TextboxConfirm(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;
+            var textBox = (TextBox) sender;
+            textBox.IsReadOnly = true;
+            textBox.BorderThickness = new Thickness(0);
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            var regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void DescriptionChange(object sender, TextChangedEventArgs textChangedEventArgs)      {
+            CurrentGame.Description = ((TextBox)sender).Text;
+        }
+        private void YearChange(object sender, TextChangedEventArgs e)
+        {
+            CurrentGame.ReleaseDate = int.Parse(((TextBox)sender).Text);
+        }
+
+        private void TitleChange(object sender, TextChangedEventArgs e)
+        {
+            CurrentGame.Title = ((TextBox)sender).Text;
+        }
+
+        private void EditorChange(object sender, TextChangedEventArgs e)
+        {
+            CurrentGame.Editor = ((TextBox)sender).Text;
+        }
+
+        private void EditCover(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png",
+                InitialDirectory = @"C:\",
+                Title = "Sélectionnez une image"
+            };
+
+            if (dialog.ShowDialog() != true) return;
+            
+            CurrentGame.CoverPath = dialog.FileName;
+            OnPropertyChanged();
         }
     }
 }
